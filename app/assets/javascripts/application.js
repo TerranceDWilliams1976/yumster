1aa8a7999cf51045b62fbdfc7c741a862d455f87
terrancedwilliams1976 <terrance.d.williams1976@gmail.com>
2018-11-11 02:33:40 +0000

    Added bootstrap to project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
